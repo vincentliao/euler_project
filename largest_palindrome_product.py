047d748d685a362795fb390fdf039797becd44e8
vincentliao <vincentliao@kkbox.com>
2014-05-03 01:09:35 +0800

    Use easier way to check palindromic number.

--- largest_palindrome_product.py
+++ largest_palindrome_product.py
@@ -8,10 +8,7 @@ from itertools import product
 # palindromic number is like 121, 9009, etc.... 
 def is_palindromic(number): 
 	number_str = str(number)
-	half_cnt = len(number_str) / 2
-	if number_str[:half_cnt] == number_str[half_cnt:][::-1]:
-		return True	
-	return False
+	return True if number_str == number_str[::-1] else False
 
 candidate = []
 for num1, num2 in product(range(999, 99, -1), range(999, 99, -1)):
